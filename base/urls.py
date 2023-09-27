bc39a39de55112526537907328ea4f946e1b4b8e
Phantonym21 <71942501+Phantonym21@users.noreply.github.com>
2023-09-27 12:58:56 +0530

    update 2

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.home,name="home"),
+    path("", views.home, name="home"),
     # path("room",views.room, name="room"),
-    path("room/<int:pk>",views.room_number, name="room_number")
+    path("room/<int:pk>", views.room_number, name="room_number"),
+    path("create-room/", views.createRoom, name="create-room"),
+    path("update-room/<str:pk>/", views.updateRoom, name="update-room"),
+    path("delete-room/<str:pk>/", views.deleteRoom, name="delete-room"),
+    path("login", views.loginPage, name="login"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,33 +1,92 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.http import HttpResponse
-from . models import Room
+from .models import Room, Topic
+from .forms import RoomForm
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
-rooms= [
-    {'id':1,'name':"CM punk"},
-    {'id':2,'name':"John Cena"},
-    {'id':3,'name':"Daniel Bryan"},
-    {'id':4,'name':"Rey Mysterio"}
+rooms = [
+    {"id": 1, "name": "CM punk"},
+    {"id": 2, "name": "John Cena"},
+    {"id": 3, "name": "Daniel Bryan"},
+    {"id": 4, "name": "Rey Mysterio"},
 ]
 
+
 def home(request):
-    rooms = Room.objects.all()
-    context = {'room':rooms}
-    return render(request,"base/home.html",context)
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)
+    )
+
+    room_count = rooms.count()
+    topics = Topic.objects.all()
+
+    context = {"room": rooms, "topics": topics, "room_count": room_count}
+    return render(request, "base/home.html", context)
+
 
 def room(request):
-    
-    return render(request,"base/room.html",{"context":"room"})
+    return render(request, "base/room.html", {"context": "room"})
 
-def room_number(request,pk):
+
+def room_number(request, pk):
     room = "No such room"
 
-    room = Room.objects.get(id = pk)
+    room = Room.objects.get(id=pk)
     # for i in rooms:
     #     if pk == i['id']:
     #         room = i
     #         break
-    context ={"room":room}
-    return render(request,"base/room.html",context)
+    context = {"room": room}
+    return render(request, "base/room.html", context)
 
 
+def createRoom(request):
+    form = RoomForm
+    if request.method == "POST":
+        # print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("home")
 
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect("home")
+    return render(request, "base/delete.html", {"obj": room})
+    # pass
+
+
+def loginPage(request):
+    if request.method=='POST':
+        username =  request.POST.get('username')
+        password =  request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request,"User doesn't exist")
+    context = {}
+    return render(request, "base/login_register.html", context)
